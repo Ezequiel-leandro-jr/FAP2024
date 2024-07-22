5c7f9aba5ee4ebb037dad2fb473940babb012184
Ezequiel Leandro Jr. <55987716+Ezequiel-leandro-jr@users.noreply.github.com>
2024-07-22 19:30:50 +0000

    atividade OO final 22_07_2024

--- Lógica de Programação/desafio_1_22_07_2024/OO.py
+++ Lógica de Programação/desafio_1_22_07_2024/OO.py
@@ -8,5 +8,12 @@ class Carro:
         self.modelo = modelo
         self.ano = ano
     
+    def ligar_motor(self):
+        print('Motor ligado!')      
+    
     def info(self):
-        return 
+        return f"Marca: {self.marca} - Modelo: {self.modelo} - Ano: {self.ano}"
+
+meu_carro = Carro('Toyota', 'Corolla', 2020)
+meu_carro.ligar_motor
+print(meu_carro.info())
