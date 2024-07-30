9e6f1256a5876cfd30250a485a84b664a9bd61a6
Ezequiel Leandro Jr. <55987716+Ezequiel-leandro-jr@users.noreply.github.com>
2024-07-30 15:17:39 +0000

    input de dados e print de resultados

--- Lógica de Programação/ruby/praticas/00.logica.rb
+++ Lógica de Programação/ruby/praticas/00.logica.rb
@@ -1,1 +1,5 @@
-puts "Olá, mundo!"
+puts "Olá! Digite seu nome:"
+nome = gets.chomp
+puts "Muito prazer em conhecê-lo, #{nome}! Agora, me diga a sua idade:"
+idade = gets.chomp.to_i
+puts "Entendi, você tem #{idade}..."
